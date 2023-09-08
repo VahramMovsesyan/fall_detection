3118fa0e2035512a2bbd7ec9823026902fe767fd
CODEinfoam <vahram.mv@mail.ru>
2023-09-09 01:31:08 +0400

    fix some bugs

--- fall_detection.py
+++ fall_detection.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 
 
 class FallDetecion():
-    def __init__(self, threshold: float | int = 5.0):
+    def __init__(self, threshold=5.0):
         self.threshold = threshold
         # Used in to_vector function
         self.specified_connections = [
@@ -18,7 +18,7 @@ class FallDetecion():
         # print(len(self.specified_connections)) # 23
         
         # Used in angle_calculator function. The last two pair for vertical vector
-        self.vec_pairs =[(18,17),(17,15),(19,20),(19,16),(11,13),(6,5),(21,23),(22,23)]
+        self.vec_pairs =[(17,16),(16,14),(18,19),(18,15),(10,11),(5,4),(20,22),(21,22)]
 
 
     def __call__(self, skeleton_cache):
@@ -33,36 +33,21 @@ class FallDetecion():
                 - bool: isFall (True or False)
                 - float: fallScore
         '''
+        self.frame_count = skeleton_cache.shape[0]
         skeleton_cache = self.fill_missing_points(skeleton_cache)
-
-
-        angles = np.array([0])
-        x = [i for i in range(1617)] # for plot
-        frm_angle = [] # for experiment
-        # print(angles,1)
-        for i in range(90):
-            for y in range(i, 18+i):
-                current_frm_vec, next_frm_vec = self.to_vector(skeleton_cache[y, :]), self.to_vector(skeleton_cache[y+1, :])
-                # print(current_frm_vec, next_frm_vec, sep='\n',end='\n======================================================\n')
-                
-                current_frm_angle, next_frm_angle = self.angle_calculator(current_frm_vec), self.angle_calculator(next_frm_vec)
-                # print(current_frm_angle, next_frm_angle, sep='\n',end='\n======================================================\n')
-                frame_angle = np.around(np.sum(np.around(np.sum(next_frm_angle - current_frm_angle) / 8, 5))/18, 5) 
-                if angles:
-                    frm_angle.append(frame_angle) # for experiment
-                    angles = np.around((angles + frame_angle) / 2, 5)
-                    # print(angles,2)
-                else:
-                    angles = frame_angle
-                    # print(angles,3)
-        plt.plot(x,frm_angle)
-        plt.xlabel("X-axis Label")
-        plt.ylabel("Y-axis Label")
-        plt.title("Simple Line Plot")
-        plt.show()
         
-        isFall = min(frm_angle) < self.threshold
-        fallScore = f'Fall Score = {torch.round(torch.sigmoid(torch.sum(torch.tensor(frm_angle, dtype=torch.float32))),decimals=4)}'
+        angles = 0
+        frm_angle = np.empty((1))
+        for i in range(self.frame_count-1):
+            current_frm_vec, next_frm_vec = self.to_vector(skeleton_cache[i, :]), self.to_vector(skeleton_cache[i+1, :])
+            current_frm_angle, next_frm_angle = self.angle_calculator(current_frm_vec), self.angle_calculator(next_frm_vec)
+            angl_diff = abs(next_frm_angle - current_frm_angle)
+            frm_angle = np.append(frm_angle,np.around(np.mean(angl_diff),5))
+        
+        
+        angles = np.round(np.mean(frm_angle),5)
+        isFall = angles > self.threshold
+        fallScore = f'Fall Score = {angles}'
         return isFall, fallScore
 
 
@@ -71,17 +56,24 @@ class FallDetecion():
 
         # Fill missing points with NaNs
         skeleton_data[skeleton_data <= 0] = np.nan
+
         try:
-            for i in range(108):
+            for i in range(self.frame_count):
                 arr = skeleton_data[i, ..., 1]
                 indices = np.arange(len(arr))
                 known_indices = indices[~np.isnan(arr)]
                 arr = np.interp(indices, known_indices, arr[known_indices])
-                skeleton_data[i, ..., 1] = arr
+                skeleton_data[i, :, 1] = arr
+    
+                arr = skeleton_data[i, :, 0]
+                indices = np.arange(len(arr))
+                known_indices = indices[~np.isnan(arr)]
+                arr = np.interp(indices, known_indices, arr[known_indices])
+                skeleton_data[i, :, 0] = arr
 
         # uses for skelethon_data_2
         except ValueError:
-            for i in range(17):
+            for i in range(skeleton_data.shape[1]): # 17
                 arr = skeleton_data[..., i, 1]
                 indices = np.arange(len(arr))
                 known_indices = indices[~np.isnan(arr)]
@@ -95,7 +87,9 @@ class FallDetecion():
                 skeleton_data[..., i, 0] = arr
 
         return skeleton_data
+    
 
+    # Create vectors from 17 + 4 points
     def to_vector(self, keypoints: np.ndarray) -> np.ndarray:
 
         # Add 4 point
@@ -114,27 +108,25 @@ class FallDetecion():
             vectors.append(vector)
 
         
-        # Add vertical vector
         vectors = np.array(vectors)  # 1d array -> 2d array
         # print(vectors.shape) # (23,2)
         return vectors
 
+    # Calculate vector's angles
     def angle_calculator(self, vectors_arr: np.ndarray) -> np.ndarray:
         
         angles = []
-        for pair in self.specified_connections:
+        for pair in self.vec_pairs:
             vector_1, vector_2 = vectors_arr[pair[0]], vectors_arr[pair[1]]
 
             dot = np.dot(vector_1, vector_2)
 
             magnitude1 = np.linalg.norm(vector_1)
             magnitude2 = np.linalg.norm(vector_2)
-
-            angle_rad = np.arccos(dot / (magnitude1 * magnitude2))
-            angles.append(np.degrees(angle_rad))
+            angle_in_radians = np.arccos(np.clip(dot / (magnitude1 * magnitude2), -1.0, 1.0))
+            angles.append(np.degrees(angle_in_radians))
 
         return np.array(angles)
-    
        
 
 # load data
@@ -143,5 +135,15 @@ skeleton_data_2 = np.load('./data/skeleton_2.npy')
 skeleton_data_3 = np.load('./data/skeleton_3.npy')
 
 
-fall_obj = FallDetecion(threshold=-1.0)
-print(fall_obj(skeleton_data_3))
+fall_obj = FallDetecion(threshold=6.1)
+
+# Array contain all fall score. This for visualization
+# arr = np.empty((1)) 
+
+# Testing algorithm like mashine. Delete for loop and give SPLITTED skslethon cache as an OBJECT argument 
+for i in range(90):
+    print(fall_obj(skeleton_data_2[i:i+19,...]))
+    # arr = np.append(arr,float(fall_obj(skeleton_data_3[i:i+19,...])[1].split(" = ")[1]))
+
+# plt.plot(arr)
+# plt.show()
